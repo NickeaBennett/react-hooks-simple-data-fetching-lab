1d5c1bfd427668c8be97b376b36f747a243dd7de
NickeaBennett <nickeabennett@gmail.com>
2022-06-07 15:29:03 -0400

    Added App component with fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+import React, {useEffect} from 'react'
+
+function App() {
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => data)
+    })
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
